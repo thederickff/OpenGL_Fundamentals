4b0cfa199794d74ebf437afb448c09e8dd7b62e4
derickfelix <derickfelix4321@gmail.com>
2017-12-21 07:12:52 -0500

    add vertex buffer and draw a triangle in OpenGL

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@
 #include <GLFW/glfw3.h>
 
 #include <iostream>
+#include <GL/gl_mangle.h>
 
 int main(int argc, char** argv) {
 
@@ -32,6 +33,17 @@ int main(int argc, char** argv) {
         std::cout << "Error" << std::endl;
 
     std::cout << glGetString(GL_VERSION) << std::endl;
+    
+    float positions[6] = {
+       -0.5f, -0.5f,
+        0.0f,  0.5f,
+        0.5f, -0.5f
+    };
+    
+    unsigned int buffer;
+    glGenBuffers(1, &buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
             
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
@@ -39,11 +51,7 @@ int main(int argc, char** argv) {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glBegin(GL_TRIANGLES);
-        glVertex2f(-0.5f, -0.5f);
-        glVertex2f( 0.0f,  0.5f);
-        glVertex2f( 0.5f, -0.5f);
-        glEnd();
+        glDrawArrays(GL_TRIANGLES, 0, 3);
         
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
