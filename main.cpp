fccec9d20a6b59dd22aea803358e2ca6bf1d45ea
derickfelix <derickfelix4321@gmail.com>
2017-12-20 19:55:20 -0500

    initial commit

---
+++ main.cpp
@@ -0,0 +1,43 @@
+/* 
+ * File:   main.cpp
+ * Author: derickfelix
+ *
+ * Created on December 20, 2017, 7:42 PM
+ */
+#include <GLFW/glfw3.h>
+
+int main(int argc, char** argv) {
+
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World!", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
