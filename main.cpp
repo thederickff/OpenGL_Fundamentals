e0411080a3a76445afc286377a0e53c141f212b8
derickfelix <derickfelix4321@gmail.com>
2018-01-06 09:19:33 -0500

    Add errors checking for debug only

--- main.cpp
+++ main.cpp
@@ -12,17 +12,27 @@
 #include <string>
 #include <sstream>
 
+
+//#define ASSERT(x) if (!(x)) _debugbreak;
+#ifdef DEBUG
+#define GLCall(x) GLClearError(); x; GLLogCall(#x, __FILE__, __LINE__);
+#else
+#define GLCall(x) x;
+#endif
 static void GLClearError()
 {
     while(glGetError() != GL_NO_ERROR);
 }
 
-static void GLCheckError()
+static bool GLLogCall(const char* function, const char* file, int line)
 {
     while(GLenum error = glGetError())
     {
-        std::cout << "[OpenGL Error] (" << error << ")" << std::endl;
+        std::cout << "[OpenGL Error] (" << error << ") " << function << " on the file "
+                << file << "at line " << line << std::endl;
+        return false;
     }
+    return true;
 }
 
 struct ShaderProgramSource
@@ -144,17 +154,17 @@ int main(int argc, char** argv) {
     };
     
     unsigned int buffer;
-    glGenBuffers(1, &buffer);
-    glBindBuffer(GL_ARRAY_BUFFER, buffer);
-    glBufferData(GL_ARRAY_BUFFER, 6 * 2 * sizeof(float), positions, GL_STATIC_DRAW);
+    GLCall(glGenBuffers(1, &buffer));
+    GLCall(glBindBuffer(GL_ARRAY_BUFFER, buffer));
+    GLCall(glBufferData(GL_ARRAY_BUFFER, 6 * 2 * sizeof(float), positions, GL_STATIC_DRAW));
     
-    glEnableVertexAttribArray(0);
-    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
+    GLCall(glEnableVertexAttribArray(0));
+    GLCall(glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0));
     
     unsigned int ibo;
-    glGenBuffers(1, &ibo);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indices, GL_STATIC_DRAW);
+    GLCall(glGenBuffers(1, &ibo));
+    GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo));
+    GLCall(glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indices, GL_STATIC_DRAW));
     
     ShaderProgramSource source = ParseShader("res/shaders/Basic.shader");
     unsigned int shader = CreateShader(source.VertexSource, source.FragmentSource);
@@ -166,9 +176,9 @@ int main(int argc, char** argv) {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-        //GLClearError();
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
-        //GLCheckError();
+        
+        GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
+        
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
 
