e164228a589aee7098f22283d576883e3edce2ef
derickfelix <derickfelix4321@gmail.com>
2018-12-14 19:52:26 -0500

    rollback changes

--- README.md
+++
@@ -1,3 +0,0 @@
-# RightWayGLFW
-The right way to start a with GLFW OpenGL library in cpp.
-I am learning it from a series of videos on Youtube channel: **TheChernoProject**.
--- main.cpp
+++ main.cpp
@@ -1,40 +1,66 @@
-/* 
- * File:   main.cpp
- * Author: derickfelix
- *
- * Created on December 20, 2017, 7:42 PM
- */
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
 #include <iostream>
-#include <fstream>
-#include <string>
-#include <sstream>
 
-#define DEBUG
+static unsigned int CompileShader(unsigned int type, const std::string& source) 
+{
+    unsigned int id = glCreateShader(type);
+    //const char* src = &source[0];
+    const char* src = source.c_str();
+    glShaderSource(id, 1, &src, nullptr);
+    glCompileShader(id);
+    
+    int result;
+    glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+    
+    if (result == GL_FALSE)
+    {
+        int length; 
+        glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+        char* message = (char*) alloca(length * sizeof(char));
+        glGetShaderInfoLog(id, length, &length, message);
+        std::cout << "Failed to compile " << (type == GL_VERTEX_SHADER ? "vertex" : "fragment")
+                << " shader!" << std::endl;
+        std::cout << message << std::endl;
+        glDeleteShader(id);
+        
+        return 0;
+    }
+        
+    
+    return id;
+}
 
-#ifdef _WIN32
-    #define alloca __builtin_alloca 
-#endif
+static unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader) 
+{
+    unsigned int program = glCreateProgram();
+    unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
+    unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
+    
+    glAttachShader(program, vs);
+    glAttachShader(program, fs);
+    glLinkProgram(program);
+    glValidateProgram(program);
+    
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+    
+    return program;
+}
 
-#include "Renderer.h"
-#include "VertexBuffer.h"
-#include "IndexBuffer.h"
-#include "VertexArray.h"
-#include "Shader.h"
-
-int main(int argc, char** argv) {
-
+int main(int argc, char** argv)
+{
     GLFWwindow* window;
 
     /* Initialize the library */
     if (!glfwInit())
         return -1;
-    
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    // glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    // glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    // glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
 
     /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
@@ -48,75 +74,68 @@ int main(int argc, char** argv) {
     glfwMakeContextCurrent(window);
 
     glfwSwapInterval(1);
-    
-    /* Initialize the library GLEW */
+
     if (glewInit() != GLEW_OK)
         return -1;
 
     std::cout << glGetString(GL_VERSION) << std::endl;
-    
+
     float positions[] = {
        -0.5f, -0.5f, // 0
+        0.0f,  0.5f, // 2
         0.5f, -0.5f, // 1
-        0.5f,  0.5f, // 2
-       -0.5f,  0.5f, // 3
     };
+
+    GLuint buffer;
+    glGenBuffers(1, &buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
+
+    std::string vertexShader = 
+            "#version 300 es\n"
+            "\n"
+            "layout(location = 0) in vec4 position;\n"
+            "\n"
+            "void main()\n"
+            "{\n"
+            " gl_Position = position;\n"
+            "}\n";
     
-    unsigned int indices[] = {
-      0, 1, 2,
-      2, 3, 0
-    };
+    std::string fragmentShader = 
+            "#version 300 es\n"
+            "\n"
+            "layout(location = 0) out mediump vec4 color;\n"
+            "\n"
+            "void main()\n"
+            "{\n"
+            " color = vec4(0.0, 1.0, 0.0, 1.0);\n"
+            "}\n";
+   
+    unsigned int shader = CreateShader(vertexShader, fragmentShader);
     
-    VertexArray va;
-    VertexBuffer vb(positions, 4 * 2 * sizeof(float));
-    // Set the layout of our vertex buffer, enable index 0 which is linked to vertex array object
-    VertexBufferLayout layout;
-    layout.Push<float>(2);
-    va.AddBuffer(vb, layout);
-
-
-    IndexBuffer ib(indices, 6);
-
-    Shader shader("res/shaders/Basic.shader");
-    shader.Bind();
-    shader.SetUniform4f("u_Color", 0.8f, 0.3f, 0.8f, 1.0f);
-
-    va.Unbind();
-    vb.Unbind();
-    ib.Unbind();
-    shader.Unbind();
-
-    float r = 0.0f;
-    float increment = 0.05f;
+    glUseProgram(shader);
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
-        GLCall(glClear(GL_COLOR_BUFFER_BIT));
-
-        shader.Bind();
-        shader.SetUniform4f("u_Color", r, 0.3f, 0.8f, 1.0f);
-
-        va.Bind();
-        ib.Bind();
-
-        GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
-
-        if (r > 1.0f)
-            increment = -0.5f;
-        else if (r < 0.0f)
-            increment = 0.05f;
-
-        r += increment;
-
+        glClear(GL_COLOR_BUFFER_BIT);
+ 
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+         
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
-
+ 
         /* Poll for and process events */
         glfwPollEvents();
     }
 
+    glDeleteProgram(shader);
+
     glfwTerminate();
+
     return 0;
 }
